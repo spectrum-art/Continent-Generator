367c356badb7ff3d5c6c39adbd44c64ba5e006e5
spectrum-art <spectrum@DESKTOP-IR6U6QP.>
2026-02-21 13:15:49 -0800

    major-3: reset repository to fresh empty baseline

--- terrain/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Terrain generation package."""
-
-from .config import DEFAULT_HEIGHT, DEFAULT_MPP, DEFAULT_WIDTH, GeneratorConfig
-
-__all__ = ["DEFAULT_WIDTH", "DEFAULT_HEIGHT", "DEFAULT_MPP", "GeneratorConfig"]
